ab189c89dfc985a43e7c608876b8f56c4f0e4278
Matías Fuentes Salinas <tarkus@manticore.xj9.com>
2019-06-07 18:05:09 -0400

    agregado pojo e hibernate

---
+++ src/java/helpers/AlumnoHelper.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import pojos.Alumno;
+import util.HibernateUtil;
+
+/**
+ *
+ * @author tarkus
+ */
+public class AlumnoHelper {
+      Session session=null;
+      List <Alumno> alumnos;
+    
+     public AlumnoHelper(){
+        this.session = HibernateUtil.getSessionFactory().openSession();
+        Query q = session.createQuery("from Usuario");
+        alumnos = (List<Alumno>) q.list();
+    }
+    
+    public void updatePersonas(){
+        
+        Query q = session.createQuery("from Usuario");
+        alumnos = (List<Alumno>) q.list();
+    }
+    
+    public List getPersonas(){
+        return alumnos;
+    }
+}
