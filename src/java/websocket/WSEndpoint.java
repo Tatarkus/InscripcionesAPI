91e8a4015712646830302ae9d533e00a8ab6d97b
Matías Fuentes Salinas <tarkus@manticore.xj9.com>
2019-06-11 18:52:54 -0400

    se pasa el json a la api

--- src/java/websocket/WSEndpoint.java
+++ src/java/websocket/WSEndpoint.java
@@ -5,9 +5,12 @@
  */
 package websocket;
 
+import com.google.gson.Gson;
 import helpers.AlumnoHelper;
+import java.util.List;
 import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
+import pojos.Alumno;
 
 /**
  *
@@ -18,10 +21,24 @@ public class WSEndpoint {
     AlumnoHelper ah = new AlumnoHelper();
     @OnMessage
     public String onMessage(String message) {
-        System.out.println("ENVIANDO MENSAJE...");
         if(message.equals("cargar")){
+            Gson gson = new Gson();
+            Alumno al = new Alumno();
+            String json = "[";
+            int contador = 0;
+            
+            for (Alumno alumno : (List<Alumno>)ah.getAlumnos()) {
+                json = json + gson.toJson(alumno)+',';
+                if(contador == 20)
+                    break;
+            }
+            json=json+']';
+            System.out.println("ENVIANDO MENSAJE: " + json);
+
+
             return String.valueOf(ah.getAlumnos().size());
         } else{
+            System.out.println("ENVIANDO MENSAJE: 0");
             return "0";
         }
     }
